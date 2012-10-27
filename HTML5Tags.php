df352a81ae52e729cc55060caed92bf30ad39819
Leo Wallentin <leo_wallentin@hotmail.com>
2012-10-28 00:30:25 +0200

    Cleanup

--- HTML5Tags.php
+++ HTML5Tags.php
@@ -17,8 +17,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-if ( version_compare( $wgVersion, '1.19', '<' ) ) {
-	die( '<b>Error:</b> This version of Semantic Result Formats requires MediaWiki 1.19 or above.' );
+if ( version_compare( $wgVersion, '1.18', '<' ) ) {
+	die( '<b>Error:</b> This version of Semantic Result Formats requires MediaWiki 1.18 or above.' );
 }
 
 define( 'HTML5TAGS_VERSION', '0.1' );
@@ -49,47 +49,4 @@ function wfHTML5TagsParserInit ( $parser ) {
 	HTML5Tags::setHooks( $parser );
 	return true;
 
-}
- 
- 
-function efSsmMicroDataTag_Render( $input, $args, $parser, $frame) {
-//var_dump($args);
-	if ( isset($args['prop']) )
-		$prop = $args['prop'];
-	elseif  ( isset($args['itemprop']) )
-		$prop = $args['itemprop'];
-	else $prop = '';
-
-        $input = $parser->recursiveTagParse( $input, $frame );
-
-	$ut = '';
-	switch ($prop) {
-		case 'published':
-				$ut="<span itemprop='published' datetime='$input'>";//TODO
-				break;
-		case 'dateModified':
-		case 'dateCreated':
-		case 'datePublished':
-				$time = new ssmDatum($input);
-				$ut='<span itemprop="dateModified" datetime="'.$time->format('c').'">';
-				break;
-		case 'prepTime':
-		case 'cookTime':
-		case 'totalTime':
-				$time=ssmDatum::newDuration($input);
-				$ut="<span itemprop='totalTime' datetime='$time'>";
-				break;
-		case 'ingredient':
-				$ut='<span itemprop="ingredient" itemscope itemtype="http://data-vocabulary.org/RecipeIngredient">';
-				break;
-		case 'photo':
-				$ut =  preg_replace('/\<img\ /', '<img itemprop="photo" ', $in, 1);
-				break;
-		default:
-				$ut="<span itemprop='$prop'>";
-				break;
-	}
-
-	$ut .= "$input</span>";
-	return $ut;
 }
--- README.md
+++
@@ -1,4 +0,0 @@
-HTML5Tags
-=========
-
-Allows a couple of HTML5 tags such as figure/figcaption in MediaWiki
