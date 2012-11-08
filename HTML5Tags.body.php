f21a71dd2f47c8a31cbbf8f4e7ae1913db76211a
mtyeh411 <dev.mtyeh411@gmail.com>
2012-11-08 14:18:08 -0500

    Add hook for aside element

--- HTML5Tags.body.php
+++ HTML5Tags.body.php
@@ -23,6 +23,7 @@ class HTML5Tags {
 		$parser->setHook( 'time', array( 'HTML5Tags', 'tagTime' ) );
 		$parser->setHook( 'footer', array( 'HTML5Tags', 'tagFooter' ) );
 		$parser->setHook( 'header', array( 'HTML5Tags', 'tagHeader' ) );
+		$parser->setHook( 'aside', array( 'HTML5Tags', 'tagAside' ) );
 		return true;
 
 	}
